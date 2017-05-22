62ef45a9007b27e01d9ad6233f187c9c019fb62e
Boris Wilhelms <mail@wille-zone.de>
2017-05-22 23:42:23 +0200

    First draft

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if isPreflightRequest(r) {
+		preflightHandler(w, r)
+		return
+	}
+
+	remoteURL := r.URL.RawQuery[len("url="):]
+	if remoteURL == "" {
+		http.NotFound(w, r)
+		return
+	}
+	request, err := http.NewRequest(r.Method, remoteURL, r.Body)
+	copyHeader(r.Header, request.Header)
+	client := &http.Client{}
+
+	fmt.Println(request.UserAgent())
+	resp, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	defer resp.Body.Close()
+
+	copyHeader(resp.Header, w.Header())
+	w.WriteHeader(resp.StatusCode)
+}
+
+func copyHeader(source http.Header, destination http.Header) {
+	for key, value := range source {
+		destination.Add(key, strings.Join(value, " "))
+	}
+}
+
+func main() {
+	os.Setenv("HTTP_PROXY", "http://localhost:8888")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
---
+++ preflight.go
@@ -0,0 +1,23 @@
+package main
+
+import "net/http"
+
+func preflightHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET HEAD POST PUT DELETE TRACE CONNECT")
+	w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
+	w.WriteHeader(http.StatusOK)
+}
+
+func isPreflightRequest(r *http.Request) bool {
+	if r.Method != "OPTIONS" {
+		return false
+	}
+
+	var accessControlRequestMethod = r.Header.Get("Access-Control-Request-Method")
+	if accessControlRequestMethod != "" {
+		return true
+	}
+
+	return false
+}
