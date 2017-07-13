19d1ae6d35e1e01b04f2fe706524b7ce2a404f75
Boris Wilhelms <mail@wille-zone.de>
2017-07-13 22:20:52 +0200

    Use Request Method from header

--- cors/preflight.go
+++ cors/preflight.go
@@ -11,11 +11,11 @@ func PreflightHandler(handler func(http.ResponseWriter, *http.Request)) func(htt
 			handler(w, r)
 			return
 		}
-		
+
 		log.Println("Handle preflight request for", r.URL)
 
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", r.Method)
+		w.Header().Set("Access-Control-Allow-Methods", r.Header.Get("Access-Control-Request-Method"))
 		w.Header().Set("Access-Control-Allow-Headers", r.Header.Get("Access-Control-Request-Headers"))
 		w.WriteHeader(http.StatusOK)
 	}
